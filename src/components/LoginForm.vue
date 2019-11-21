a4a9aa285a84cf8d6e99f44751a770fe750d6a11
GyuReeKim <starkim96@naver.com>
2019-11-21 23:16:58 +0900

    signup 구성중

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <div v-if="loading" class="spinner-border" role="status">
+      <span class="sr-only">Loading...</span>
+    </div>
+    <div class="login-div col-6 offset-3">
+      <div class="form-group">
+        <label for="id">ID</label>
+        <input id="id" class="form-control" type="text" v-model="credential.username">
+      </div>
+      <div class="form-group">
+        <label for="password">PASSWORD</label>
+        <input id="password" class="form-control" type="password" v-model="credential.password">
+      </div>
+      <button class="btn btn-primary" @click="login">로그인</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'LoginForm',
+  data: function(){
+    return {
+      credential: {
+        username: '',
+        password: '',
+      },
+      loading: false
+    }
+  },
+  methods: {
+    login(){
+      console.log('로그인 시도!!!')
+      axios.post('http://localhost:8000/api-token-auth/', this.credential)
+      .then((res)=>{
+        this.loading = true
+        console.log(res)
+      })
+      .catch((error)=>{
+        console.log(error)
+      })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/SignupForm.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <div v-if="loading" class="spinner-border" role="status">
+      <span class="sr-only">Loading...</span>
+    </div>
+    <div class="signup-div col-6 offset-3">
+      <div class="form-group">
+        <label for="id">ID</label>
+        <input id="id" class="form-control" type="text" v-model="credential.username">
+      </div>
+      <div class="form-group">
+        <label for="password1">PASSWORD</label>
+        <input id="password1" class="form-control" type="password" v-model="credential.password1">
+      </div>
+      <div class="form-group">
+        <label for="password2">PASSWORD 확인</label>
+        <input id="password2" class="form-control" type="password" v-model="credential.password2">
+      </div>
+      <div class="form-group">
+        <label for="email">PASSWORD</label>
+        <input id="email" class="form-control" type="email" v-model="credential.email">
+      </div>
+      <button class="btn btn-primary" @click="signup">회원가입</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'SignupForm',
+  data: function(){
+    return {
+      credential: {
+        username: '',
+        password1: '',
+        password2: '',
+        email: ''
+      },
+      loading: false
+    }
+  },
+  methods: {
+    signup(){
+      console.log('회원가입 시도!!!')
+      axios.post('http://localhost:8000', this.credential)
+      .then((res)=>{
+        console.log(res)
+        
+      })
+      .catch((error)=>{
+        console.log(error)
+      })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
